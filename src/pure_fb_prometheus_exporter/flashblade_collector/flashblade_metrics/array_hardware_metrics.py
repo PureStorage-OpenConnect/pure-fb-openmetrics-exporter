6aeb04eed488addac678fcb08d1032b570323e14
Eugenio Grosso <eugenio.grosso@gmail.com>
2022-02-03 07:33:56 +0000

    Removed usage of regex for labeling hw components metrics

--- src/pure_fb_prometheus_exporter/flashblade_collector/flashblade_metrics/array_hardware_metrics.py
+++ src/pure_fb_prometheus_exporter/flashblade_collector/flashblade_metrics/array_hardware_metrics.py
@@ -1,4 +1,3 @@
-import re
 from prometheus_client.core import GaugeMetricFamily
 
 
@@ -18,14 +17,7 @@ class ArrayHardwareMetrics():
         self.array_hardware_status = GaugeMetricFamily(
                                   'purefb_hardware_health',
                                   'FlashBlade hardware component health status',
-                                   labels=['chassis', 'fabric_module', 'type',
-                                           'name', 'index', 'slot'])
-        re_ch = re.compile(r"^(CH\d+)$")
-        re_fb = re.compile(r"^(CH\d+)\.(FB[0-9]+)$")
-        re_fm = re.compile(r"^(CH\d+)\.(FM[0-9]+)$")
-        re_eth = re.compile(r"^(CH\d+)\.(FM\d+)\.ETH([0-9]+)(\.[0-9]+)?$")
-        re_fan = re.compile(r"^(CH\d+)\.(FM\d+)\.FAN([0-9]+)$")
-        re_pwr = re.compile(r"^(CH\d+)\.(PWR[0-9]+)$")
+                                   labels=['type', 'name', 'index', 'slot'])
 
         for comp in self.hardware:
             if comp.status == 'not_installed':
@@ -33,8 +25,6 @@ class ArrayHardwareMetrics():
             c_name = comp.name
             c_index = comp.index or ''
             c_slot = comp.slot or ''
-            chassis = ''
-            fabric_module = ''
             
             if comp.status == 'healthy':
                 c_state = 1
@@ -42,33 +32,8 @@ class ArrayHardwareMetrics():
                 c_state = 2
             else:
                 c_state = 0
-
             c_type = comp.type
-            if c_type == 'ch':               # Chassis
-                detail = re_ch.match(c_name)
-                chassis = detail.group(1)
-            elif c_type == 'fb':             # Flash Blade
-                detail = re_fb.match(c_name)
-                chassis = detail.group(1)
-            elif c_type == 'fm':             # Fabric Module
-                detail = re_fm.match(c_name)
-                chassis = detail.group(1)
-                fabric_module = detail.group(2)
-            elif c_type == 'eth':            # Ethernet card
-                detail = re_eth.match(c_name)
-                chassis = detail.group(1)
-                fabric_module = detail.group(2)
-            elif c_type == 'fan':            # Fan
-                detail = re_fan.match(c_name)
-                chassis = detail.group(1)
-                fabric_module = detail.group(2)
-            elif c_type == 'pwr':            # Power
-                detail = re_pwr.match(c_name)
-                chassis = detail.group(1)
-
-            self.array_hardware_status.add_metric([chassis, 
-                                                   fabric_module,
-                                                   c_type,
+            self.array_hardware_status.add_metric([c_type,
                                                    c_name,
                                                    str(c_index),
                                                    str(c_slot)], c_state)

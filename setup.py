82613a4ef2275b93c779a28947ff188a06585573
Eugenio Grosso <eugenio.grosso@gmail.com>
2022-02-16 23:29:04 +0000

    Modified Dockerfiles to match renamed files

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-    Setup file for pure-fb-prometheus-exporter.
+    Setup file for pure-fb-openmetrics-exporter.
     Use setup.cfg to configure your project.
 
     This file was generated with PyScaffold 4.1.1.

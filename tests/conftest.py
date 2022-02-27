57ce8a843f646bd05057a22435631fb8c34382aa
Eugenio Grosso <eugenio.grosso@purestorage.com>
2022-02-27 11:40:27 +0100

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,8 +7,8 @@ from pure_fb_openmetrics_exporter.flashblade_client import client
 def fb_client(scope="session"):
     try:
         c = client.FlashbladeClient(
-                             target = '10.225.112.69',
-                             api_token='T-2b74f9eb-a35f-40d9-a6a6-33c13775a53c',
+                             target = '10.11.12.1',  # put your fb mgnt ip here
+                             api_token='T-xxxxxxxx-yyyy-zzzz-kkkk-jjjjjjjjjjjj', # your API token
                              disable_ssl_warn = True)
     except PureError as pe:
         pytest.fail("Could not connect to flashblade {0}".format(pe))

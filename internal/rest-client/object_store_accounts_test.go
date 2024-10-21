b150cb557825d01678a882139986bf849a07771e
Christopher Roberts <chrroberts@purestorage.com>
2024-10-21 11:35:04 -0500

    docs scaffolding

--- internal/rest-client/object_store_accounts_test.go
+++ internal/rest-client/object_store_accounts_test.go
@@ -34,7 +34,7 @@ func TestObjectStoreAccounts(t *testing.T) {
 	e := endp[len(endp)-1]
 	t.Run("ojectstoreaccounts_1", func(t *testing.T) {
 		defer server.Close()
-		c := NewRestClient(e, "fake-api-token", "latest", "test-user-agent-string", false)
+		c := NewRestClient(e, "fake-api-token", "latest", "test-user-agent-string", false, false)
 		osal := c.GetObjectStoreAccounts()
 		if diff := cmp.Diff(osal.Items, osa.Items); diff != "" {
 			t.Errorf("Mismatch (-want +got):\n%s", diff)

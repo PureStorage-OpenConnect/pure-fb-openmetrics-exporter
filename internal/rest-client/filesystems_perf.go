38b6c1a71c13fcbf6a48ddedb02e988ae1c9ddfb
Tyler Muir <tylergmuir@gmail.com>
2025-08-14 10:32:05 -0700

    fix error with more than 10 file systems

--- internal/rest-client/filesystems_perf.go
+++ internal/rest-client/filesystems_perf.go
@@ -1,5 +1,7 @@
 package client
 
+import "strings"
+
 type FileSystemsPerformanceList struct {
 	CntToken     string        `json:"continuation_token"`
 	TotalItemCnt int           `json:"total_item_count"`
@@ -11,18 +13,31 @@ func (fb *FBClient) GetFileSystemsPerformance(f *FileSystemsList,
 	protocol string) *FileSystemsPerformanceList {
 	uri := "/file-systems/performance"
 	result := new(FileSystemsPerformanceList)
+	const chunkSize = 10
+
 	switch protocol {
 	case "all", "NFS", "SMB":
-		res, _ := fb.RestClient.R().
-			SetResult(&result).
-			SetQueryParam("protocol", protocol).
-			Get(uri)
-		if res.StatusCode() == 401 {
-			fb.RefreshSession()
-			fb.RestClient.R().
-				SetResult(&result).
+		for i := 0; i < len(f.Items); i += chunkSize {
+			names := make([]string, 0, chunkSize)
+			for _, fs := range f.Items[i:min(i+chunkSize, len(f.Items))] {
+				names = append(names, fs.Name)
+			}
+			n := strings.Join(names, ",")
+			temp := new(FileSystemsPerformanceList)
+			res, _ := fb.RestClient.R().
+				SetResult(&temp).
+				SetQueryParam("names", n).
 				SetQueryParam("protocol", protocol).
 				Get(uri)
+			if res.StatusCode() == 401 {
+				fb.RefreshSession()
+				fb.RestClient.R().
+					SetResult(&temp).
+					SetQueryParam("names", n).
+					SetQueryParam("protocol", protocol).
+					Get(uri)
+			}
+			result.Items = append(result.Items, temp.Items...)
 		}
 	}
 	return result
